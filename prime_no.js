90428d817ed9d4da66496f8507872ae2f1031f03
Deepak Rajput <70367179+dpkraj@users.noreply.github.com>
2022-10-22 21:39:11 +0530

    Create prime_no.js

---
+++ prime_no.js
@@ -0,0 +1,13 @@
+let num = 12; 
+let count = 0;
+    for(let x = 1 ; x<=num;x++){
+        
+        if(num%x==0){
+        count ++;
+        }
+         }if(count==2){
+         
+        console.log("Yes") 
+        } else{
+             console.log("No")
+       } 
